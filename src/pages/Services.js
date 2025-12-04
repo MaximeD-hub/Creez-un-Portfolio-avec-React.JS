2f0aaa9ef664dd8c169ad1daaf1b49a4917b01fd
MaximeD <one.maximeduboispro@gmail.com>
2025-12-04 14:00:42 +0100

    portfolio et services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,9 +4,9 @@ import bannerImg from "../assets/images/banner.jpg";
 
 function Services() {
   const services = [
-    { title: "Service 1", description: "Description du service 1" },
-    { title: "Service 2", description: "Description du service 2" },
-    { title: "Service 3", description: "Description du service 3" },
+    { title: "UX Design", description: "L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciel, objets connectés,etc...) en plaçant l'utilisateur au centre des préoccupations. L'Objectif est de rendre l'expérience utilisateur le plus fluide et agréable possible.", icon:"bi-brush"},
+    { title: "Développement Web", description: "Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, Javascript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)", icon: "bi-code-slash" },
+    { title: "Référencement", description: "Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site.", icon:"bi-search" },
   ];
 
   return (
@@ -21,12 +21,14 @@ function Services() {
 
       <section className="py-5">
         <Container>
-          <h2 className="text-center mb-2">Services</h2>
-          <h3 className="text-center mb-5">Découvrez mes offres</h3>
+          <h2 className="text-center mb-2">Mon offre de services</h2>
+          <h3 className="text-center mb-5">Voici les prestations sur lesquelles je peux intervenir</h3>
           <Row>
             {services.map((service, idx) => (
               <Col md={4} key={idx} className="mb-4">
                 <Card className="h-100 text-center">
+                  <i className={`${service.icon} mb-3`} style={{ fontSize: "3rem", color: "blue"  }}
+                  ></i>
                   <Card.Body>
                     <Card.Title>{service.title}</Card.Title>
                     <Card.Text>{service.description}</Card.Text>
